c29658de0e0b2ca48decc62423f51cf6d93626fc
Jeremy Neptune <jerenept@gmail.com>
2015-03-16 20:00:46 -0400

    added some models
    added models for user, post and comment. we should consider what other
    models we may need and what they would require; this is just a start

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	name = models.CharField(max_length=200)
+	email = models.EmailField()
+	#no password field as I would encourage the use of Facebook login in lieu of inconvenient accounts
+	
+class Post(models.Model):
+	
+	post_type_choices = (('buy','Buying'), ('sell','Selling'))
+	
+	post_type = models.CharField(max_length = 4, choices = post_type_choices, default='sell')
+	poster = models.ForeignKey('User')
+	post_text = models.TextField(max_length=1500)
+	sale_category = models.CharField(max_length=75)
+	post_time = models.DateField()
+	post_last_edited = models.DateField()
+	price = models.IntegerField()
+	availability = models.BooleanField()
+	
+	
+class Comment(models.Model):
+	poster = models.ForeignKey('User')
+	post = models.ForeignKey('Post')
+	comment_text = models.TextField(max_length=1500)
+	comment_time = 	models.DateField()	
+	comment_last_edited = models.DateField()
+	
