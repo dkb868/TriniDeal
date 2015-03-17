3d1f2f3050f7aa6ba22d822204c7ef8743ab17b9
Dmitri <mitrikyle@yahoo.com>
2015-03-17 03:37:16 -0400

    Fixed up shop and static media
    Changed models a lot
    Added Static Media Server
    Made a little index
    Made a view for that index
    Added a test image to test the static media server

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls import patterns, url, include
-
+from django.conf.urls import patterns, url
 from shop import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.example, name='example'),
+#	url(r'^$', views.example, name='example'),
 	)
