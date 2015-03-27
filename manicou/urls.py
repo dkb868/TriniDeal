ec4186db12be88beeb3d4bf3fe69aca9fac51690
Dmitri <mitrikyle@yahoo.com>
2015-03-27 18:33:21 -0400

    some templates added tests pasin

--- manicou/urls.py
+++ manicou/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic import TemplateView
 import notifications
 
 
@@ -11,6 +12,10 @@ urlpatterns = patterns('',
     url(r'^review/', include('review.urls')),
     url(r'^inbox/notifications/', include(notifications.urls)),
     url(r'^shop/', include('shop.urls', namespace='shop')),
+    url(r'^about/$', TemplateView.as_view(template_name='main/about.html'), name='about'),
+  #  url(r'contact^$', 'shop.views.contact', name='contact'),
+
+
 )
 
 # development media server
--- shop/views.py
+++ shop/views.py
@@ -13,9 +13,9 @@ def index(request):
 
 def category(request, category_name_slug):
 	category = Category.objects.get(slug=category_name_slug)
-	item_list = SaleItem.objects.filter(category=category)
+	item_list = SaleItem.objects.filter(category=category).order_by('post_time')[:9]
 	context_dict = {'category': category, 'items': item_list}
-	return render(request, 'main/index.html', context_dict)
+	return render(request, 'shop/category.html', context_dict)
 
 def saleitem(request, item_slug):
 	item = SaleItem.objects.get(slug=item_slug)
@@ -57,7 +57,7 @@ def add_new_item(request):
 def sellerprofile(request, user_id):
 	sellerprofile = User.objects.get(id=user_id).sellerprofile
 	context_dict = {'sellerprofile': sellerprofile}
-	return render(request, 'main/sellerprofile.html', context_dict)
+	return render(request, 'shop/sellerprofile.html', context_dict)
 
 
 def make_bid(request, item_slug):
