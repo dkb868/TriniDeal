d7cf31a0097037b1d78ec945b25e03f6e4fc79ff
Dmitri <mitrikyle@yahoo.com>
2015-03-31 00:39:00 -0400

    Most templates reasonable. forms still need ui

--- manicou/settings.py
+++ manicou/settings.py
@@ -97,8 +97,17 @@ WSGI_APPLICATION = 'manicou.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'manicou_db',
+        #'HOST': 'mysql.server',
+       # 'HOST': '127.0.0.1',
+      # 'PORT': '3300',
+        'USER': 'root',
+        'PASSWORD': 'Allthatiknow1',
+        'OPTIONS':{
+        	"init_command": "SET foreign_key_checks = 0;",
+        },
     }
 }
 
